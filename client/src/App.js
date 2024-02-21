cecef0b4586fc5dda34b85bff51c501bc85dc3c8
Kons Zogr <konszogr@hotmail.com>
2024-02-21 13:09:03 +0200

    App title updated

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React and Test AWS
+            Fib Calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
